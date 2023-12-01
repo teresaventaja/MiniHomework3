4f5484113c61b24dddde76f30c7174a6df071672
Teresa Ventaja <71657946+teresaventaja@users.noreply.github.com>
2023-12-01 20:07:21 +0000

    Change2
    Printing2

--- src/minihomework3/MiniHomework3.java
+++ src/minihomework3/MiniHomework3.java
@@ -16,6 +16,7 @@ public class MiniHomework3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Printing1");
+        System.out.println("Printing2");
     }
     
 }
